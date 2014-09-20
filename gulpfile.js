8b91fa04c17a577a29214c7f358dd507693e61a2
John Macaluso <macaluso.john@gmail.com>
2014-09-20 09:10:36 -0400

    added svgstore

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,40 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    minifycss = require('gulp-minify-css'),
-    prefix = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload'),
-    imagemin = require('gulp-imagemin');
-    //Todo - Add svgstore
-    //Todo - Add JS minifiy
-    //Todo - Add combine-media-queries
+   sass = require('gulp-ruby-sass'),
+   minifycss = require('gulp-minify-css'),
+   uglify = require('gulp-uglify'),
+   prefix = require('gulp-autoprefixer'),
+   livereload = require('gulp-livereload'),
+   svgmin = require('gulp-svgmin'),
+   svgstore = require('gulp-svgstore');
+   //TODO:  Add js minify
 
 gulp.task('sass', function() {
-  return gulp.src('sass/style.scss')
-    .pipe(sass({ style: 'compressed' }))
-    .pipe(minifycss())
-    .pipe(prefix({ cascade: true }))
-    .pipe(gulp.dest('./'));
+   return gulp.src('sass/style.scss')
+      .pipe(sass({ style: 'compressed' }))
+      .pipe(minifycss())
+      .pipe(prefix({ cascade: true }))
+      .pipe(gulp.dest('./'));
 });
 
-gulp.task('images', function() {
-  return gulp.src('img/**/*')
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('img'))
-    .pipe(notify({ message: 'Images task complete' }));
+gulp.task('compress', function() {
+   gulp.src('js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('js/dist'))
+});
+
+gulp.task('svg', function(){
+   return gulp.src('svg/*.svg')
+      .pipe(svgmin())
+      .pipe(svgstore({ 
+         fileName: 'icons.svg',
+         prefix: 'icon-',
+         inlineSvg: true,
+         transformSvg: function transformSvg (svg, cb) {
+            svg.attr({ style: 'display:none' })
+            cb(null)
+         }
+      }))
+      .pipe(gulp.dest('img/svg-sprite/'))
 });
 
 gulp.task('default', function() {
@@ -28,10 +42,13 @@ gulp.task('default', function() {
    //livereload
    livereload.listen();
 
+   // SVG Sprite
+   gulp.watch('svg/*.svg',['svg']);
+
    // Watch .scss files
    gulp.watch('sass/**/*.scss', ['sass']);
 
-   // Watch image files
-   gulp.watch('img/**/*', ['images']);
+   // Watch .js files
+   gulp.watch('js/*.js', ['compress']);
 
 });
