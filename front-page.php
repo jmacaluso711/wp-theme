241afc0fd2b896b0b001639edc82458049ad18cf
John Macaluso <macaluso.john@gmail.com>
2014-10-25 08:54:22 -0400

    added front page

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+   <h2><?php the_title(); ?></h2>
+   <?php the_content(); ?>
+   <?php endwhile; ?>
+
+<?php get_footer(); ?>
