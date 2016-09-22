df9226168a08ff370a308178738409dd47d162c6
John Macaluso <john.macaluso@ms-ds.com>
2016-09-22 17:36:51 -0400

    bower components

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -26,13 +26,13 @@
         wp_enqueue_script('jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.min.js', array(), '3.1.0', true);
 
         //Plugins
-        wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr-min/dist/modernizr.min.js', array(), '3.2.0', false );
+        // wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr-min/dist/modernizr.min.js', array(), '3.2.0', false );
 
         //Custom Scripts
         wp_register_script( 'main', get_template_directory_uri() . '/build/js/main.js', array(), '1.0.0', true );
 
         //Enqueue Scripts
-        wp_enqueue_script('modernizr');
+        // wp_enqueue_script('modernizr');
         wp_enqueue_script('jquery');
         wp_enqueue_script('main');
 
