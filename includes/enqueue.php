c0674cbeb1e690f8b363c5b92f9348acf7c3d947
John Macaluso <macaluso.john@gmail.com>
2018-06-25 20:32:40 -0400

    fix merge conflicts

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -7,11 +7,7 @@
     // Header Scripts
     function header_scripts() {
 
-<<<<<<< HEAD
     }
-=======
-     }
->>>>>>> d5f99e8250adb58fdf8beb135d909bc9614a6bb9
     add_action('wp_head', 'header_scripts');
 
     // Footer Scripts
@@ -21,7 +17,6 @@
     add_action('wp_footer', 'footer_scripts');
 
     function theme_scripts() {
-<<<<<<< HEAD
       //Stylesheets
       wp_enqueue_style( 'screen', get_stylesheet_uri() );
       wp_enqueue_style( 'main', get_template_directory_uri().'/dist/css/main.css' );
@@ -40,26 +35,6 @@
       // wp_enqueue_script('modernizr');
       wp_enqueue_script('jquery');
       wp_enqueue_script('main');
-=======
-        //Stylesheets
-        wp_enqueue_style( 'screen', get_stylesheet_uri() );
-        wp_enqueue_style( 'main', get_template_directory_uri().'/build/css/main.css' );
-
-        //jQuery
-        wp_deregister_script('jquery');
-        wp_enqueue_script('jquery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.min.js', array(), '3.1.0', true);
-
-        //Plugins
-        // wp_register_script( 'modernizr', get_template_directory_uri() . '/bower_components/modernizr-min/dist/modernizr.min.js', array(), '3.2.0', false );
-
-        //Custom Scripts
-        wp_register_script( 'main', get_template_directory_uri() . '/build/js/main.js', array(), '1.0.0', true );
-
-        //Enqueue Scripts
-        // wp_enqueue_script('modernizr');
-        wp_enqueue_script('jquery');
-        wp_enqueue_script('main');
->>>>>>> d5f99e8250adb58fdf8beb135d909bc9614a6bb9
 
     }
 
